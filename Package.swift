b46a70a58a4edfac272b1fed788cf21330346763
Thomas Evensen <thomeven@gmail.com>
2026-01-14 14:03:10 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,13 +1,11 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 6.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "RsyncAnalyse",
-    platforms: [
-        .macOS(.v14)
-    ],
+    platforms: [.macOS(.v14)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -19,10 +17,7 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "RsyncAnalyse",
-            swiftSettings: [
-                .enableUpcomingFeature("StrictConcurrency")
-            ]
+            name: "RsyncAnalyse"
         ),
         .testTarget(
             name: "RsyncAnalyseTests",
